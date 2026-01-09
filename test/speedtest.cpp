f521a8bc54ccd8d3aa810e07e5fabb78ad107f42
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2026-01-09 16:13:27 +0100

    Update speedtest.cpp
    readability

--- test/speedtest.cpp
+++ test/speedtest.cpp
@@ -9,6 +9,8 @@
 #include <sched.h>
 #include <cstring>
 #include <memory>
+#include <sstream>
+#include <chrono>
 
 #include "Latte.hpp"
 
@@ -16,6 +18,7 @@ template <typename T>
 static inline void do_not_optimize(T const& value) {
     asm volatile("" : : "r,m"(value));
 }
+
 void PinThread(int core_id) {
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
@@ -25,8 +28,6 @@ void PinThread(int core_id) {
         std::cerr << "Warning: Failed to pin thread. Benchmarks may be unstable.\n";
     }
 }
-
-
 
 static __inline__ uint64_t __attribute__((always_inline)) rdtsc_begin() {
     _mm_lfence();
@@ -38,8 +39,6 @@ static __inline__ uint64_t __attribute__((always_inline)) rdtsc_end() {
     _mm_lfence();
     return res;
 }
-
-
 
 constexpr int ITERATIONS = 100000;
 constexpr int SAMPLES = 100;
@@ -54,11 +53,10 @@ struct BenchResult {
         auto samples_ptr = std::make_unique<double[]>(SAMPLES); \
         double* samples = samples_ptr.get(); \
         \
-        /* Warmup: Run without timing to populate I-Cache/D-Cache/TLB */ \
-        /* Volatile loop prevents compiler from removing "useless" code */ \
+        /* Warmup */ \
         for(volatile int w=0; w < (ITERATIONS); ++w) { CODE_BLOCK } \
         \
-        uint64_t start, end; /* alloc cold path */\
+        uint64_t start, end; \
         \
         for(int s=0; s < SAMPLES; ++s) { \
             start = rdtsc_begin(); \
@@ -66,12 +64,10 @@ struct BenchResult {
                 CODE_BLOCK \
             } \
             end = rdtsc_end(); \
-            \
-            /* delta after end beacon */ \
             samples[s] = (double)(end - start) / ITERATIONS; \
         } \
         \
-        /* Statistics*/ \
+        /* Statistics */ \
         std::sort(samples, samples + SAMPLES); \
         double sum = 0.0; \
         for(int i=0; i<SAMPLES; ++i) sum += samples[i]; \
@@ -89,50 +85,64 @@ struct BenchResult {
         return std::make_unique<BenchResult>(BenchResult{avg, med, min, max, std_dev, NAME}); \
     })()
 
-void PrintResult(const BenchResult& r, double baseline) {
-    // Pure Latency = Measured - EmptyLoopCost
-    double pure_cost = r.med - baseline;
-    if (pure_cost < 0) pure_cost = 0;
 
-    std::stringstream ss;
-    ss << std::fixed << std::setprecision(1);
-    ss << "Range: [" << r.min << " - " << r.max << "]";
-    std::string rangeStr = ss.str();
-    std::cout << "| " << std::left << std::setw(25) << r.name
-              << "| Total: " << std::fixed << std::setprecision(1) << std::setw(6) << r.med
-              << " | Overhead: " << "\033[1;34m" << std::setw(6) << pure_cost << "\033[0m"
-              << " | " << std::left << std::setw(25) << rangeStr << " |" << std::endl;
+void PrintResult(const BenchResult& r, double baseline) {
+    // Pure Latency calculations (subtracting latency of ACTION(CODE_BLOCK -> TARGET_CODE))
+    double adj_med = (r.med - baseline) > 0 ? (r.med - baseline) : 0.0;
+    double adj_avg = (r.avg - baseline) > 0 ? (r.avg - baseline) : 0.0;
+    double adj_min = (r.min - baseline) > 0 ? (r.min - baseline) : 0.0;
+    double adj_max = (r.max - baseline) > 0 ? (r.max - baseline) : 0.0;
+
+    double delta  = r.max - r.min;
+
+    std::cout << "| " << std::left  << std::setw(23) << r.name
+              << " | " << std::right << std::fixed << std::setprecision(1) << std::setw(8) << r.med // Raw Total
+              << " | " << "\033[1;34m" << std::setw(8) << adj_med << "\033[0m" // Cost (Blue)
+              << " | " << std::setw(8) << adj_avg
+              << " | " << std::setw(8) << adj_min
+              << " | " << std::setw(8) << adj_max
+              << " | " << std::setw(8) << r.std_dev
+              << " | " << std::setw(8) << delta
+              << " |" << std::endl;
 }
 
-
 int main() {
-    PinThread(2); // isolated from OS interrupts
+    PinThread(2);
 
-    std::cout << "+=========================================================================================+\n";
-    std::cout << "| LATTE LATENCY BENCHMARK (Cycles per Operation)                                          |\n";
-    std::cout << "+=========================================================================================+\n";
+    std::cout << "+======================================================================================================+" << std::endl;
+    std::cout << "| LATTE LATENCY BENCHMARK (Cycles per Operation)                                                       |" << std::endl;
+    std::cout << "+======================================================================================================+" << std::endl;
 
-    // Measure Baseline (Empty Loop Cost)
     auto r_baseline = BENCHMARK("Baseline (Empty Loop)", {
         asm volatile("");
     });
 
-    // Curiosity
-    auto r_rdtsc = BENCHMARK("__rdtsc", {
-        do_not_optimize(__rdtsc());
-    });
-    auto r_rdtscp = BENCHMARK("__rdtscp", {
-        unsigned int aux;
-        do_not_optimize(__rdtscp(&aux));
-    });
-    auto r_LFENCE = BENCHMARK("_LFENCE", {
-        _mm_lfence();
-    });
+    std::cout << "| Loop Overhead Baseline: \033[1;34m" << std::fixed << std::setprecision(2) << r_baseline->med << "\033[0m cycles/iter                                                             |" << std::endl;
+    std::cout << "+=========================+==========+==========+==========+==========+==========+==========+==========+" << std::endl;
 
-    std::cout << "| Loop Overhead detected: \033[1;34m" << r_baseline->med << "\033[0m cycles/iter                                             |\n";
-    std::cout << "+-----------------------------------------------------------------------------------------+\n";
+    // Table Header
+    std::cout << "| " << std::left  << std::setw(23) << "Benchmark Name"
+              << " | " << std::right << std::setw(8) << "Total"
+              << " | " << std::right << std::setw(8) << "Cost"
+              << " | " << std::right << std::setw(8) << "Avg"
+              << " | " << std::right << std::setw(8) << "Min"
+              << " | " << std::right << std::setw(8) << "Max"
+              << " | " << std::right << std::setw(8) << "StdDev"
+              << " | " << std::right << std::setw(8) << "Delta"
+              << " |" << std::endl;
+    std::cout << "+-------------------------+----------+----------+----------+----------+----------+----------+----------+" << std::endl;
 
-    // Measure Start+Stop PAIRS (::stop() expect values fetched from ::start())
+
+    auto r_rdtsc = BENCHMARK("__rdtsc", { do_not_optimize(__rdtsc()); });
+    auto r_rdtscp = BENCHMARK("__rdtscp", { unsigned int aux; do_not_optimize(__rdtscp(&aux)); });
+    auto r_LFENCE = BENCHMARK("_LFENCE", { _mm_lfence(); });
+
+    PrintResult(*r_rdtsc, r_baseline->med);
+    PrintResult(*r_rdtscp, r_baseline->med);
+    PrintResult(*r_LFENCE, r_baseline->med);
+    std::cout << "+-------------------------+----------+----------+----------+----------+----------+----------+----------+" << std::endl;
+
+
     auto r_fast = BENCHMARK("Fast::Start + Stop", {
         Latte::Fast::Start("BenchFast");
         Latte::Fast::Stop(nullptr);
@@ -148,29 +158,25 @@ int main() {
         Latte::Hard::Stop(nullptr);
     });
 
-    // Measure PULSE (The Optimized Loop Method)
+    PrintResult(*r_fast, r_baseline->med);
+    PrintResult(*r_mid, r_baseline->med);
+    PrintResult(*r_hard, r_baseline->med);
+    std::cout << "+-------------------------+----------+----------+----------+----------+----------+----------+----------+" << std::endl;
+
+
     auto r_pulse = BENCHMARK("LATTE_PULSE (Loop)", {
         LATTE_PULSE("BenchPulse");
     });
+    PrintResult(*r_pulse, r_baseline->med);
+    std::cout << "+-------------------------+----------+----------+----------+----------+----------+----------+----------+" << std::endl;
 
-    // Against std::chrono::now()
+
     auto r_chrono = BENCHMARK("std::chrono::now()", {
          auto t1 = std::chrono::high_resolution_clock::now();
          auto cd = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - t1).count();
     });
-
-
-    PrintResult(*r_rdtsc, r_baseline->med);
-    PrintResult(*r_rdtscp, r_baseline->med);
-    PrintResult(*r_LFENCE, r_baseline->med);
-    std::cout << "+-----------------------------------------------------------------------------------------+\n";
-    PrintResult(*r_fast, r_baseline->med);
-    PrintResult(*r_mid, r_baseline->med);
-    PrintResult(*r_hard, r_baseline->med);
-    std::cout << "+-----------------------------------------------------------------------------------------+\n";
-    PrintResult(*r_pulse, r_baseline->med);
-    std::cout << "+-----------------------------------------------------------------------------------------+\n";
     PrintResult(*r_chrono, r_baseline->med);
-    std::cout << "+=========================================================================================+" << std::endl;
+    std::cout << "+-------------------------+----------+----------+----------+----------+----------+----------+----------+" << std::endl;
+
     return 0;
 }
